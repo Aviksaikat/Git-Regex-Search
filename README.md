b05bd25e8b4cb0b053a5a6e281504fd39758e318
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2023-01-28 20:19:45 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Git Search Using Regex
+
+> Saikat Karmakar | 28 Jan:2023
+
+---
+
+This program allows you to search for a specific pattern within the files of a GitHub repository([Using PyGithub](https://pygithub.readthedocs.io/en/latest/)). The program utilizes regular expressions and multithreading to quickly search through the repository's contents and return all files that contain the specified pattern.
+
+# Features
+- Search for a specific pattern in the files of a GitHub repository
+- Supports single repository or file containing multiple repository URLs
+- Uses multithreading to improve search performance
+
+
+# Installation
+To use this program, you will need to have Python 3 and the PyGithub library installed. You can install PyGithub using pip:
+```bash
+pip install PyGithub
+pip install pytoml
+```
+
+You will also need to have a GitHub personal access token. You can create one by going to the [GitHub Developer Settings](https://github.com/settings/tokens).
+
+# Usage
+To use the program, you will need to provide a GitHub access token. The token can be passed as an environment variable or in a config.toml file. The program will look for the token in the following order:
+
+- `GITHUB_TOKEN` environment variable 
+- `config.toml` file
+
+```bash
+python3 main.py -h                                              
+usage: main.py [-h] [-u URL] [-r REGEX]
+
+options:
+  -h, --help            show this help message and exit
+  -u URL, --url URL     URL of the repository (single or file
+                        containing URLs)
+  -r REGEX, --regex REGEX
+                        Regex pattern to search for
+```
+
+For example, to search for the pattern `brownie` in the repository `Aviksaikat/RoadClosed-quillctf-brownie`, you would run the following command:
+
+```bash
+python3 main.py -u https://github.com/Aviksaikat/RoadClosed-quillctf-brownie -r "brownie"
+```
+
+![](assets/img.png)
+
+
+The program will then return a list of all files that contain the specified pattern, along with the line number where the pattern was found.
+
+# Multithreading
+The program utilizes multithreading to search through the repository's contents more quickly. Two threads are created and run simultaneously, each searching through the repository's contents. This allows the program to search through the repository's contents much faster than if it were only using a single thread.
+
+# Regular Expressions
+The program utilizes regular expressions to search for the specified pattern within the files of the repository. This allows for more powerful and flexible searches, as opposed to simple string matching.
+
+# Limitations
+This program only searches the contents of the files and not the name of the files. For my use I don't need it to search the names.
+
+# Additional Features
+The program also prints the whole file path where the pattern was found, which is helpful in identifying the location of the pattern.
+
+# Conclusion
+This program is a powerful tool for quickly searching through the contents of a GitHub repository and finding specific patterns. With the use of regular expressions and multithreading, it is able to search through large repositories quickly and efficiently.
+
---
+++ main.py
@@ -0,0 +1,118 @@
+import argparse
+import threading
+import os
+import toml
+import re
+from github import Github
+
+
+# https://pygithub.readthedocs.io/en/latest/examples/Repository.html
+def process_env():
+    # Initialize the Github instance using your access token
+    
+    # Fetch the access token
+    access_token = os.environ.get("GITHUB_TOKEN")
+    #print(access_token)
+    
+    #* if not in env then fetch from config.toml file
+    if access_token is None:
+        try:
+            with open("config.toml") as config_file:
+                config = toml.load(config_file)
+                access_token = config["github"]["access_token"]
+        except (FileNotFoundError, KeyError):
+            print("Error: Could not find access token in environment variable or config.toml file.")
+            exit(1)
+    g = Github(access_token)
+    return g
+    
+
+def search_repo(repository, pattern) -> tuple:
+    g = process_env()
+    org = repository.split('/')[-2]
+    repo_name = repository.split('/')[-1]
+    repo = g.get_repo(org + '/' + repo_name)
+    results = []
+
+    def search_contents(contents):
+        for item in contents:
+            if item.type == "dir":
+                search_contents(repo.get_contents(item.path))
+            elif item.type == "file":
+                file_contents = repo.get_contents(item.path).decoded_content
+                for i, line in enumerate(file_contents.splitlines(), 1):
+                    if re.search(pattern.encode(), line):
+                        results.append((item, i))
+
+    thread1 = threading.Thread(target=search_contents, args=(repo.get_contents(""),))
+    thread2 = threading.Thread(target=search_contents, args=(repo.get_contents(""),))
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+
+    #print(results)
+    return results
+
+
+
+def main():
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser()
+
+    # Add the repository name argument
+    #parser.add_argument("repo", required=False, help="Name of the repository to search in")
+
+    # Add the URL option argument
+    parser.add_argument("-u", "--url", help="URL of the repository (single or file containing URLs)")
+
+    # Add the regex argument
+    parser.add_argument("-r", "--regex", help="Regex pattern to search for")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+
+    # Set the pattern to search for
+    if args.regex:
+        pattern = args.regex
+    else:
+        pattern = "latestRoundData"
+
+    if args.url:
+        if args.url.endswith(".txt"):
+            # The URL is a file containing multiple URLs
+            with open(args.url, "r") as file:
+                urls = file.readlines()
+                for url in urls:
+                    try:
+                        # repo = g.get_repo(url.strip())
+                        # results = repo.search_code(pattern)
+                        results = search_repo(url.strip(), pattern)
+                        for result in results:
+                            print(f"{url.strip()}/tree/master/{result[0].path}#L{result[1]}")
+                    except Exception as e:
+                        print(f"Error: {e}")
+        else:
+            # The URL is a single repository
+            try:
+                # repo = g.get_repo(args.url)
+                # results = repo.search_code(pattern)
+                results = search_repo(args.url, pattern)
+                for result in results:
+                    print(f"{args.url}/tree/master/{result[0].path}#L{result[1]}")
+            except Exception as e:
+                print(f"Error: {e}")
+    else:
+        try:
+            # repo = g.get_repo(args.repo)
+            # results = repo.search_code(pattern)
+            results = search_repo(args.repo, pattern)
+            for result in results:
+                print(f"{args.repo}/tree/master/{result[0].path}#L{result[1]}")
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+if __name__ == '__main__':
+    main()
