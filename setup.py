410c1e352e5db4817a46b9faf70850eab982fd57
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2023-10-17 20:35:03 +0100

    Auto-commit changes

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-from setuptools import setup, find_packages
 import codecs
 import os
+
+from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     LONG_DESCRIPTION = "\n" + fh.read()
 
-CLASSIFIERS=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Education",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+CLASSIFIERS = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Education",
+    "Programming Language :: Python :: 3",
+    "Operating System :: Unix",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+]
 
 DESCRIPTION = "This program allows you to search for a specific pattern within the files of a GitHub repository(using PyGithub)."
 # LONG_DESCRIPTION = "This program allows you to search for a specific pattern within the files of a GitHub repository(using PyGithub). The program utilizes regular expressions and multithreading to quickly search through the repository's contents and return all files that contain the specified pattern."
@@ -28,11 +29,6 @@ setup(
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[
-        "toml",
-        "re",
-        "PyGithub",
-        "termcolor"
-    ],
+    install_requires=["toml", "re", "PyGithub", "termcolor"],
     classifiers=CLASSIFIERS,
 )
